010cd17ab3edcb28eb2974a7c7804b752ade1bf0
Daniel Hutmacher <daniel@strd.co>
2025-04-10 14:11:48 +0200

    Added Azure Web App health check

--- server.js
+++ server.js
@@ -106,6 +106,17 @@ app.get('/', function (req, res, next) {
 
 
 
+/*-----------------------------------------------------------------------------
+  Azure Linux App Service Plan health check request:
+  ---------------------------------------------------------------------------*/
+
+app.get('/robots933456.txt', function (req, res, next) {
+    console.log("Azure health check: OK.");
+    res.status(200).send("OK");
+});
+
+
+
 
 /*-----------------------------------------------------------------------------
   Other related assets, like images, CSS or other files.
